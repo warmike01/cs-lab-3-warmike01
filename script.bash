8114a3e619b93201ff188b607618b4416a99f7ac
Mikhail <368306@niuitmo.ru>
2022-10-23 23:29:25 +0300

    попытка 1

--- script.bash
+++ script.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 export LANG=en_US.UTF-8
-
+d=0
+f=0
 function print()
 {
 local subdirs=($1/*)
@@ -13,16 +14,40 @@ for counter in ${!subdirs[@]}; do
 
 	if [ -d ${subdirs[$counter]} ]
 	then 
+		((d++))
 		echo -n "├── "
-		
 		echo ${subdirs[$counter]##*/}
 		print ${subdirs[$counter]} $(($c+1))
 	elif [ -f ${subdirs[$counter]} ]
 	then
+		((f++))
 		echo -n "└── "
 		echo ${subdirs[$counter]##*/}
+		
 	fi
 done
 }
 
-print $1 0
+if [ -z "$1" ]
+then
+	echo '.'
+	print '.' 0
+else
+	echo $1
+	print $1 0
+fi
+printf '\n'
+echo -n $d
+if [ $d -eq 1 ]
+then
+echo -n " directory, "
+else
+echo -n " directories, "
+fi
+echo -n $f
+if [ $f -eq 1 ]
+then
+echo " file"
+else
+echo " files"
+fi
